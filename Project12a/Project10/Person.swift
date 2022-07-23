9e4f4ff556228a5968831e520891e9c1b11ed35c
Gökhan Cihan <cihangokhan@gmail.com>
2022-07-23 13:19:54 +0300

    person class conforms NSCoding

--- Project12a/Project10/Person.swift
+++ Project12a/Project10/Person.swift
@@ -7,12 +7,18 @@
 
 import UIKit
 
-class Person: NSObject {
+class Person: NSObject, NSCoding {
     var name: String
     var image: String
     
-    init(name: String, image: String) {
-        self.name = name
-        self.image = image
+    required init(coder aDecoder: NSCoder) {
+        name = aDecoder.decodeObject(forKey: "name") as? String ?? ""
+        image = aDecoder.decodeObject(forKey: "image") as? String ?? ""
     }
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: "name")
+        aCoder.encode(image, forKey: "image")
+    }
+    
 }
