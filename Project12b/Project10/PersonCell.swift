29640078471454c24496edd5f1763aaea92b8cfd
Gökhan Cihan <cihangokhan@gmail.com>
2022-07-23 15:11:37 +0300

    copied from project10

---
+++ Project12b/Project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Gökhan on 16.07.2022.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
