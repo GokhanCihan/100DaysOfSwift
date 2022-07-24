7a79bc9302b7ed47866365f2e2dbcaf64878e0d5
Gökhan Cihan <cihangokhan@gmail.com>
2022-07-24 23:10:33 +0300

    shows times viewed

---
+++ Project1/Project1/PicturesCell.swift
@@ -0,0 +1,23 @@
+//
+//  PicturesCell.swift
+//  Project1
+//
+//  Created by Gökhan on 24.07.2022.
+//
+
+import UIKit
+
+class PicturesCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Project1/Project1/ViewController.swift
+++ Project1/Project1/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UITableViewController {
     var pictures = [String]()
+    var viewsCount = [Int]()
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -16,10 +17,6 @@ class ViewController: UITableViewController {
         
         navigationController?.navigationBar.prefersLargeTitles = true
         
-        performSelector(inBackground: #selector(loadPicturesFromBundle), with: nil)
-    }
-    
-    @objc func loadPicturesFromBundle() {
         let fm = FileManager.default
                 let path = Bundle.main.resourcePath!
                 let items = try! fm.contentsOfDirectory(atPath: path)
@@ -29,12 +26,21 @@ class ViewController: UITableViewController {
                         pictures.append(item)
                     }
                 }
-        pictures = pictures.sorted()
-        performSelector(onMainThread: #selector(pushIntoMain), with: nil, waitUntilDone: false)
-    }
-    
-    @objc func pushIntoMain(){
-        tableView.reloadData()
+        for _ in 0..<pictures.count{
+            viewsCount.append(0)
+        }
+        
+        let defaults = UserDefaults.standard
+        
+        if let loadData = defaults.object(forKey: "viewsCount") as? Data {
+            let jsonDecoder = JSONDecoder()
+            
+            do {
+                viewsCount = try jsonDecoder.decode([Int].self, from: loadData)
+            } catch {
+                print("Load failed")
+            }
+        }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -43,17 +49,30 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
-        cell.textLabel?.text = pictures[indexPath.row]
+        cell.textLabel?.text = "\(pictures[indexPath.row]): viewed \(viewsCount[indexPath.row]) times"
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let removedCount = viewsCount.remove(at: indexPath.row)
+        viewsCount.insert(removedCount + 1, at: indexPath.row)
+        self.save()
+        self.tableView.reloadData()
         if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
             vc.selectedImage = pictures[indexPath.row]
             vc.imagesTitle = "Picture \(indexPath.row+1) of \(pictures.count)"
             navigationController?.pushViewController(vc, animated: true)
-            
         }
     }
-
+    
+    func save() {
+        let jsonEncoder = JSONEncoder()
+        
+        if let savedData = try? jsonEncoder.encode(viewsCount) {
+            let defaults = UserDefaults.standard
+            defaults.set(savedData, forKey: "viewsCount")
+        } else {
+            print("Save Failed")
+        }
+    }
 }
