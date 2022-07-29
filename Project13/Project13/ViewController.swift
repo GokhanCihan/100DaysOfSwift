38e12b13e14d6e6d88b30579c994c45aee39a9a5
Gökhan Cihan <cihangokhan@gmail.com>
2022-07-29 17:22:21 +0300

    storyboard adjusted

---
+++ Project13/Project13/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Project13
+//
+//  Created by Gökhan on 29.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var intensity: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+    
+    @IBAction func Save(_ sender: Any) {
+    }
+    
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
+}
+
