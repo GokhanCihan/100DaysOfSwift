7ba7cf786a60d21560742a3b0da3200d3de9e71c
Gökhan Cihan <cihangokhan@gmail.com>
2022-07-29 17:51:52 +0300

    imagePickerController

--- Project13/Project13/ViewController.swift
+++ Project13/Project13/ViewController.swift
@@ -7,15 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var intensity: UISlider!
+    var currentImage: UIImage!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        title = "Image Filter"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
     }
 
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else {return}
+        
+        dismiss(animated: true)
+        currentImage = image
+    }
+    
     @IBAction func changeFilter(_ sender: Any) {
     }
     
@@ -24,5 +42,6 @@ class ViewController: UIViewController {
     
     @IBAction func intensityChanged(_ sender: Any) {
     }
+    
 }
 
