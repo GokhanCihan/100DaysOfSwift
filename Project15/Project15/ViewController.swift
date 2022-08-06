e44b9373b270cede8fae5ee96a1f792466d07d80
Gökhan Cihan <cihangokhan@gmail.com>
2022-08-06 12:34:30 +0300

    scaling, rotating and translating with CGAffineTransform

--- Project15/Project15/ViewController.swift
+++ Project15/Project15/ViewController.swift
@@ -16,26 +16,49 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         imageView = UIImageView(image: UIImage(named: "penguin"))
-        imageView.center = CGPoint(x: 512, y: 384)
+        imageView.center = CGPoint(x: 658, y: 514)
         view.addSubview(imageView)
     }
 
-    
-    
     @IBAction func Tapped(_ sender: UIButton) {
         sender.isHidden = true
         
-        UIView.animate(withDuration: 1, delay: 0, options: [],
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, options: [],
                        animations: {
             switch self.currentAnimation {
             case 0:
-                break
+                self.imageView.transform = CGAffineTransform(scaleX: 2, y: 2)
+                
+            case 1:
+                self.imageView.transform = .identity
+                
+            case 2:
+                self.imageView.transform = CGAffineTransform(translationX: -256, y: -256)
+                
+            case 3:
+                self.imageView.transform = .identity
+                
+            case 4:
+                self.imageView.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+                
+            case 5:
+                self.imageView.transform = .identity
+                
+            case 6:
+                self.imageView.alpha = 0.1
+                self.imageView.backgroundColor = UIColor.green
+                
+            case 7:
+                self.imageView.alpha = 1
+                self.imageView.backgroundColor = UIColor.clear
+                
             default:
                 break
             }
         }) { finished in
             sender.isHidden = false
         }
+        
         currentAnimation += 1
         
         if currentAnimation > 7 {
