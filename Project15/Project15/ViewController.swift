38e02d2df46fbcbdcfd3ec4f801bce9a6a80c9ca
Gökhan Cihan <cihangokhan@gmail.com>
2022-08-06 12:02:15 +0300

    animate(withDuration:)

--- Project15/Project15/ViewController.swift
+++ Project15/Project15/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var imageView: UIImageView!
+    var currentAnimation = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        imageView = UIImageView(image: UIImage(named: "penguin"))
+        imageView.center = CGPoint(x: 512, y: 384)
+        view.addSubview(imageView)
     }
 
-
+    
+    
+    @IBAction func Tapped(_ sender: UIButton) {
+        sender.isHidden = true
+        
+        UIView.animate(withDuration: 1, delay: 0, options: [],
+                       animations: {
+            switch self.currentAnimation {
+            case 0:
+                break
+            default:
+                break
+            }
+        }) { finished in
+            sender.isHidden = false
+        }
+        currentAnimation += 1
+        
+        if currentAnimation > 7 {
+            currentAnimation = 0
+        }
+    }
+    
 }
 
