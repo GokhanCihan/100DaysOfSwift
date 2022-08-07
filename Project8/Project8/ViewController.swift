ebae430952dd44211a990b88b99893bee4334f81
Gökhan Cihan <cihangokhan@gmail.com>
2022-08-07 23:12:26 +0300

    Tapped buttons fade out

--- Project8/Project8/ViewController.swift
+++ Project8/Project8/ViewController.swift
@@ -151,7 +151,10 @@ class ViewController: UIViewController {
         guard let buttonTitle = sender.titleLabel?.text else {return}
         currentAnswer.text = currentAnswer.text?.appending(buttonTitle)
         activatedButtons.append(sender)
-        sender.isHidden = true
+        UIView.animate(withDuration: 1, delay: 0, options: [],
+                       animations: {
+            sender.alpha = 0
+        })
     }
     
     @objc func submitButtonTapped(){
@@ -187,7 +190,7 @@ class ViewController: UIViewController {
         currentAnswer.text = ""
         
         for btn in activatedButtons {
-            btn.isHidden = false
+            btn.alpha = 1
         }
         
         activatedButtons.removeAll()
@@ -246,7 +249,7 @@ class ViewController: UIViewController {
         
         
         for btn in letterButtons {
-            btn.isHidden = false
+            btn.alpha = 1
         }
     }
 }
