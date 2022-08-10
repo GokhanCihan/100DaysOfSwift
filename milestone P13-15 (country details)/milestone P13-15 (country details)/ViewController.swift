0179942b5ea96101b55f0798f235b511307800ad
Gökhan Cihan <cihangokhan@gmail.com>
2022-08-11 00:00:11 +0300

    country names in tableview

--- milestone P13-15 (country details)/milestone P13-15 (country details)/ViewController.swift
+++ milestone P13-15 (country details)/milestone P13-15 (country details)/ViewController.swift
@@ -7,8 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
     var countries = [Country]()
+    var countryNames = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -24,15 +25,30 @@ class ViewController: UIViewController {
             }
     }
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return countries.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "\(countryNames[indexPath.row])"
+        return cell
+    }
+
     func parseData(json: Data) {
         let dataDecoder = JSONDecoder()
         do {
             let jsonCountries = try dataDecoder.decode([Country].self, from: json)
-            print(jsonCountries)
+            countries = jsonCountries
+            
+            for i in 0..<countries.count {
+                countryNames.append(countries[i].name.common)
+            }
+            
+            print(jsonCountries.count)
         } catch{
             print(error)
         }
-        
     }
 }
 
