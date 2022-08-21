2246b60e951c044fe6fce1c2e524ff99dec1a3d8
Gökhan Cihan <cihangokhan@gmail.com>
2022-08-22 02:12:52 +0300

    project16 created

---
+++ Project16/Project16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project16
+//
+//  Created by Gökhan on 22.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
