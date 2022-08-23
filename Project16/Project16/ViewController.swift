ef5741bb45ce8c0eab64ee4245aba9b67c9c9f0a
Gökhan Cihan <cihangokhan@gmail.com>
2022-08-23 13:43:17 +0300

    2nd challenge

--- Project16/Project16/ViewController.swift
+++ Project16/Project16/ViewController.swift
@@ -10,6 +10,7 @@ import MapKit
 
 class ViewController: UIViewController, MKMapViewDelegate {
     @IBOutlet var mapView: MKMapView!
+    @IBOutlet var mapTypeButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,11 +20,28 @@ class ViewController: UIViewController, MKMapViewDelegate {
         let paris = Capital(title: "Paris", coordinate: CLLocationCoordinate2D(latitude: 48.8567, longitude: 2.3508), info: "Often called the City of Light.")
         let rome = Capital(title: "Rome", coordinate: CLLocationCoordinate2D(latitude: 41.9, longitude: 12.5), info: "Has a whole country inside it.")
         let washington = Capital(title: "Washington DC", coordinate: CLLocationCoordinate2D(latitude: 38.895111, longitude: -77.036667), info: "Named after George himself.")
-        
+        mapView.mapType = .satellite
         mapView.addAnnotations([london, oslo, paris, rome, washington])
 
     }
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        let ac = UIAlertController(title: "Choose map type:", message: nil, preferredStyle: .actionSheet)
+    
+        ac.addAction(UIAlertAction(title: "standart", style: .default) { [weak ac, weak self] _ in
+            self?.mapView.mapType = .standard
+            self?.mapTypeButton.setTitle("standart", for: .normal)
+        })
+        ac.addAction(UIAlertAction(title: "satellite", style: .default) { [weak ac, weak self] _ in
+            self?.mapView.mapType = .satellite
+            self?.mapTypeButton.setTitle("satellite", for: .normal)
+        })
+        ac.addAction(UIAlertAction(title: "hybrid", style: .default) { [weak ac, weak self] _ in
+            self?.mapView.mapType = .hybrid
+            self?.mapTypeButton.setTitle("hybrid", for: .normal)
+        })
+        present(ac, animated: true)
+    }
+    
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         //any annotation other than ours (from apple services etc.) will return nill
         guard annotation is Capital else { return nil }
@@ -32,7 +50,7 @@ class ViewController: UIViewController, MKMapViewDelegate {
         //dequeue annotation view from unused views
         let annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
         annotationView.canShowCallout = true
-        annotationView.pinTintColor = UIColor.magenta
+        annotationView.pinTintColor = UIColor.yellow
         let btn = UIButton(type: .detailDisclosure)
         annotationView.rightCalloutAccessoryView = btn
        
