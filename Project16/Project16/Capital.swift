2ddc1fdf6941dcee20c8b030b66748b0588a6246
Gökhan Cihan <cihangokhan@gmail.com>
2022-08-23 12:08:31 +0300

    project + first challenge

---
+++ Project16/Project16/Capital.swift
@@ -0,0 +1,21 @@
+//
+//  Capital.swift
+//  Project16
+//
+//  Created by Gökhan on 22.08.2022.
+//
+
+import UIKit
+import MapKit
+
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    init(title: String?, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+}
