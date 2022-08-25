56021f189b7e9ef8b0109b8460506ab7349311b3
Gökhan Cihan <cihangokhan@gmail.com>
2022-08-25 13:27:51 +0300

    project raw

---
+++ Project17-Space Race/Project17-Space Race/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  Project17-Space Race
+//
+//  Created by Gökhan on 25.08.2022.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        
+    }
+}
+
---
+++ Project17-Space Race/Project17-Space Race/GameViewController.swift
@@ -0,0 +1,19 @@
+//
+//  GameViewController.swift
+//  Project17-Space Race
+//
+//  Created by Gökhan on 25.08.2022.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
