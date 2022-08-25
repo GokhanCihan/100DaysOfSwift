8bdfc969a76ebaf8c6d5cd151a6f2a4e90ea299a
Gökhan Cihan <cihangokhan@gmail.com>
2022-08-25 14:41:36 +0300

    moves touched location, handles collisions

--- Project17-Space Race/Project17-Space Race/GameScene.swift
+++ Project17-Space Race/Project17-Space Race/GameScene.swift
@@ -75,5 +75,30 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         sprite.physicsBody?.linearDamping = 0
         sprite.physicsBody?.angularDamping = 0
     }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //print(touches)
+        guard let touch = touches.first else { return }
+        var location = touch.location(in: self)
+        //print(location)
+        
+        if location.y < 100 {
+               location.y = 100
+           } else if location.y > 668 {
+               location.y = 668
+           }
+
+        player.position = location
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        let explosion = SKEmitterNode(fileNamed: "explosion")!
+        explosion.position = player.position
+        addChild(explosion)
+
+        player.removeFromParent()
+
+        isGameOver = true
+    }
 }
 
