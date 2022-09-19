1726888669caed3d62d0353dfbac68767377f488
Gökhan Cihan <cihangokhan@gmail.com>
2022-09-19 23:22:58 +0300

    tableViewCell

---
+++ Notes App/Notes App/NoteCell.swift
@@ -0,0 +1,17 @@
+//
+//  NoteCell.swift
+//  Notes App
+//
+//  Created by Gökhan on 19.09.2022.
+//
+
+import UIKit
+
+class NoteCell: UITableViewCell {
+
+    @IBOutlet var cellNoteTitle: UILabel!
+    
+    @IBOutlet var cellDateEdited: UILabel!    
+    
+    @IBOutlet var cellBodyText: UILabel!
+}
--- Notes App/Notes App/ViewController.swift
+++ Notes App/Notes App/ViewController.swift
@@ -7,11 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //add number of rows to be shown
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "NoteCell", for: indexPath) as? NoteCell else {
+            fatalError("Unable to dequeue NoteCell")
+        }
+        //title and other visuals for each cell
+        cell.cellNoteTitle.text = "Title"
+        cell.cellBodyText.text = "Text Body"
+        cell.cellDateEdited.text = "Date"
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            //add properties that must be passed into detail view
+            navigationController?.pushViewController(vc, animated: true)
+        }
     }
 
 
