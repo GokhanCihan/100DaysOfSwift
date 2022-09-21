a34ba23dbb11dff00637355f2342fe8dbbe98f12
Gökhan Cihan <cihangokhan@gmail.com>
2022-09-22 02:30:51 +0300

    prototype cell for notes

---
+++ Project22-Detect-a-Beacon/Project22-Detect-a-Beacon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project22-Detect-a-Beacon
+//
+//  Created by Gökhan on 22.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
