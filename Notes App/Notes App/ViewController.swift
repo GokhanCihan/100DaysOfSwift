9ca62c1f0d1884f7c6dba3bb3ddf27b31d8435cf
Gökhan Cihan <cihangokhan@gmail.com>
2022-09-28 01:10:04 +0300

    create new folder with name

--- Notes App/Notes App/ViewController.swift
+++ Notes App/Notes App/ViewController.swift
@@ -26,7 +26,7 @@ class ViewController: UITableViewController {
             UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
         )
         toolbar.append(
-            UIBarButtonItem(image: UIImage(systemName: "folder.badge.plus"), style: .plain, target: self, action: #selector(createNewFolder))
+            UIBarButtonItem(image: UIImage(systemName: "folder.badge.plus"), style: .plain, target: self, action: #selector(askFolderName))
         )
         toolbarItems = toolbar
         
@@ -42,7 +42,8 @@ class ViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "FolderCell", for: indexPath) as? FolderCell else {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "FolderCell", for: indexPath) as? FolderCell
+        else {
             fatalError("Unable to dequeue FolderCell")
         }
         //title and other visuals for each cell
@@ -57,9 +58,22 @@ class ViewController: UITableViewController {
             navigationController?.pushViewController(vc, animated: true)
         }
     }
-
-    @objc func createNewFolder() {
-        let newFolder = Folder(savedFolderName: "created folder", savedNotes: [Note]())
+    
+    @objc func askFolderName() {
+        let ac = UIAlertController(title: "New Folder", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let askName = UIAlertAction(title: "Create", style: .default) { [weak self, weak ac] action in
+            guard let input = ac?.textFields?[0].text else { return }
+            self?.createNewFolder(withFolderName: input)
+        }
+        
+        ac.addAction(askName)
+        present(ac, animated: true)
+    }
+    
+    func createNewFolder(withFolderName: String) {
+        let newFolder = Folder(savedFolderName: withFolderName, savedNotes: [Note]())
         folders.append(newFolder)
         save()
         tableView.reloadData()
@@ -88,6 +102,5 @@ class ViewController: UITableViewController {
             }
         }
     }
-
 }
 
