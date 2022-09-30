92d30f68587e9f269b94f079594ad1ddadefb8de
Gökhan Cihan <cihangokhan@gmail.com>
2022-09-30 21:12:44 +0300

    project created

---
+++ Project23-Swifty Ninja/Project23-Swifty Ninja/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  Project23-Swifty Ninja
+//
+//  Created by Gökhan on 30.09.2022.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+}
