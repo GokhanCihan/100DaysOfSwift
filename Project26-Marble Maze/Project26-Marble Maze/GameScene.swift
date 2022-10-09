59e62d58858b92afe1410f9a28f368f4f34b6901
Gökhan Cihan <cihangokhan@gmail.com>
2022-10-09 21:25:31 +0300

    project 26 created

---
+++ Project26-Marble Maze/Project26-Marble Maze/GameScene.swift
@@ -0,0 +1,17 @@
+//
+//  GameScene.swift
+//  Project26-Marble Maze
+//
+//  Created by Gökhan on 9.10.2022.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+
+    }
+    
+}
