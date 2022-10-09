66416ce50f6cf0450e7683d5da66775f89670baf
Gökhan Cihan <cihangokhan@gmail.com>
2022-10-09 22:54:48 +0300

    CMMotionManager, compiler instructions

--- Project26-Marble Maze/Project26-Marble Maze/GameScene.swift
+++ Project26-Marble Maze/Project26-Marble Maze/GameScene.swift
@@ -5,10 +5,17 @@
 //  Created by Gökhan on 9.10.2022.
 //
 
+import CoreMotion
 import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    
+    var motionManager: CMMotionManager!
+    
+    var player: SKSpriteNode!
+    
+    var lastTouchPosition: CGPoint?
     
     enum CollisionTypes: UInt32 {
         case player = 1
@@ -20,6 +27,11 @@ class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
         
+        motionManager = CMMotionManager()
+        motionManager.startAccelerometerUpdates()
+        
+        physicsWorld.gravity = .zero
+        
         let background = SKSpriteNode(imageNamed: "background.jpg")
         background.position = CGPoint(x: 512, y: 384)
         background.blendMode = .replace
@@ -27,9 +39,60 @@ class GameScene: SKScene {
         addChild(background)
         
         loadLevel()
+        
+        createPlayer()
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        lastTouchPosition = location
+    }
+
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        lastTouchPosition = location
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        lastTouchPosition = nil
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        
+        #if targetEnvironment(simulator)
+            if let currentTouch = lastTouchPosition {
+                let diff = CGPoint(x: currentTouch.x - player.position.x, y: currentTouch.y - player.position.y)
+                physicsWorld.gravity = CGVector(dx: diff.x / 100, dy: diff.y / 100)
+            }
+        #else
+            if let accelerometerData = motionManager.accelerometerData {
+                physicsWorld.gravity = CGVector(dx: accelerometerData.acceleration.y * -50, dy: accelerometerData.acceleration.x * 50)
+            }
+        #endif
+        
+    }
+    
+    func createPlayer() {
+        
+        player = SKSpriteNode(imageNamed: "player")
+        player.position = CGPoint(x: 96, y: 672)
+        player.zPosition = 1
+        player.physicsBody = SKPhysicsBody(circleOfRadius: player.size.width / 2)
+        player.physicsBody?.allowsRotation = false
+        player.physicsBody?.linearDamping = 0.5
+
+        player.physicsBody?.categoryBitMask = CollisionTypes.player.rawValue
+        player.physicsBody?.contactTestBitMask = CollisionTypes.star.rawValue | CollisionTypes.vortex.rawValue | CollisionTypes.finish.rawValue
+        player.physicsBody?.collisionBitMask = CollisionTypes.wall.rawValue
+        addChild(player)
+        
     }
     
     func loadLevel() {
+        
         guard let levelURL = Bundle.main.url(forResource: "level1", withExtension: "txt") else {
             fatalError("Could not find level1.txt in the app bundle.")
         }
@@ -94,6 +157,7 @@ class GameScene: SKScene {
                 } else {
                     fatalError("Unknown level letter: \(letter)")
                 }
+                
             }
         }
     }
