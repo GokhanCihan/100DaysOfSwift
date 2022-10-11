a62f9e17e7eff9ebbff88d3765a8c3f13a631c5e
Gökhan Cihan <cihangokhan@gmail.com>
2022-10-11 22:01:43 +0300

    UIGraphicsImageRenderer, cgContext

--- Project27-Core Graphics/Project27-Core Graphics/ViewController.swift
+++ Project27-Core Graphics/Project27-Core Graphics/ViewController.swift
@@ -9,11 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imageView: UIImageView!
+    
+    @IBAction func redrawTapped(_ sender: Any) {
+        currentDrawType += 1
+
+        if currentDrawType > 5 {
+            currentDrawType = 0
+        }
+
+        switch currentDrawType {
+        case 0:
+            drawRectangle()
+
+        default:
+            break
+        }
+    }
+    
+    var currentDrawType = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        drawRectangle()
+        
     }
 
+    func drawRectangle() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+
+        let img = renderer.image { ctx in
+            let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512)
+
+            ctx.cgContext.setFillColor(UIColor.red.cgColor)
+            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+            ctx.cgContext.setLineWidth(10)
+
+            ctx.cgContext.addRect(rectangle)
+            ctx.cgContext.drawPath(using: .fillStroke)
+        }
+
+        imageView.image = img
+    }
 
 }
 
