75875abdc1692d4fde3a55b031ef99a315936f4e
Gökhan Cihan <cihangokhan@gmail.com>
2022-10-12 16:23:40 +0300

    drawing lines, images and text, transforming

--- Project27-Core Graphics/Project27-Core Graphics/ViewController.swift
+++ Project27-Core Graphics/Project27-Core Graphics/ViewController.swift
@@ -21,7 +21,14 @@ class ViewController: UIViewController {
         switch currentDrawType {
         case 0:
             drawRectangle()
-
+        case 1:
+            drawCircle()
+        case 2:
+            drawCheckerboard()
+        case 3:
+            drawRotatedSquares()
+        case 4:
+            drawImageAndText()
         default:
             break
         }
@@ -40,7 +47,7 @@ class ViewController: UIViewController {
         let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
 
         let img = renderer.image { ctx in
-            let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512)
+            let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512).insetBy(dx: 5, dy:5)
 
             ctx.cgContext.setFillColor(UIColor.red.cgColor)
             ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
@@ -52,6 +59,92 @@ class ViewController: UIViewController {
 
         imageView.image = img
     }
+    
+    func drawCircle() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
 
+            
+        let img = renderer.image { ctx in
+            let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512).insetBy(dx: 5, dy: 5)
+
+            ctx.cgContext.setFillColor(UIColor.red.cgColor)
+            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+            ctx.cgContext.setLineWidth(10)
+
+            ctx.cgContext.addEllipse(in: rectangle)
+            ctx.cgContext.drawPath(using: .fillStroke)
+        }
+
+        imageView.image = img
+    }
+    
+    func drawCheckerboard() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+
+        let img = renderer.image { ctx in
+            ctx.cgContext.setFillColor(UIColor.black.cgColor)
+
+            for row in 0 ..< 8 {
+                for col in 0 ..< 8 {
+                    if (row + col) % 2 == 0 {
+                    ctx.cgContext.fill(CGRect(x: col * 64, y: row * 64, width: 64, height: 64))
+                    }
+                }
+            }
+        }
+
+        imageView.image = img
+    }
+
+    func drawRotatedSquares() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+
+        let img = renderer.image { ctx in
+            ctx.cgContext.translateBy(x: 256, y: 256)
+
+            let rotations = 16
+            let amount = Double.pi / Double(rotations)
+
+            for _ in 0 ..< rotations {
+                ctx.cgContext.rotate(by: CGFloat(amount))
+                ctx.cgContext.addRect(CGRect(x: -128, y: -128, width: 256, height: 256))
+            }
+
+            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+            ctx.cgContext.strokePath()
+        }
+
+        imageView.image = img
+    }
+    
+    func drawImageAndText() {
+        //1. renderer size
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+
+        let img = renderer.image { ctx in
+            
+            //2. paragraph and text style
+            let paragraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.alignment = .center
+            
+            //3. add style into a attributes dictionary
+            let attrs: [NSAttributedString.Key: Any] = [
+                .font: UIFont.systemFont(ofSize: 48),
+                .paragraphStyle: paragraphStyle
+            ]
+            
+            //4. wrap dict and a string into an instance of NSAttributedString
+            let string = "The best-laid schemes o'\nmice an' men gang aft agley"
+            let attributedString = NSAttributedString(string: string, attributes: attrs)
+            
+            //5. load an image and draw into context
+            let mouse = UIImage(named: "mouse")
+            
+            attributedString.draw(with: CGRect(x: 32, y: 32, width: 200, height: 600), options: .usesLineFragmentOrigin, context: nil)
+            mouse?.draw(at: CGPoint(x: 300, y: 150))
+        }
+        //6. update imageview
+        imageView.image = img
+    }
 }
 
