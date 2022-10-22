e48ea444751ce4d543002edd8f94dc55ce6ea83b
Gökhan Cihan <cihangokhan@gmail.com>
2022-10-22 05:13:49 +0300

    save and load with keychain

--- Project28-Secret-Swift/Project28-Secret-Swift/ViewController.swift
+++ Project28-Secret-Swift/Project28-Secret-Swift/ViewController.swift
@@ -14,13 +14,40 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "Nothing to see here"
+        
         let notificationCenter = NotificationCenter.default
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
         
+        //adds an observer for running app on the background or switching to another app
+        notificationCenter.addObserver(self, selector: #selector(saveSecretMessage), name: UIApplication.willResignActiveNotification, object: nil)
         
     }
 
+    func unlockSecretMessage() {
+        secret.isHidden = false
+        title = "Secret Text!"
+
+        if let text = KeychainWrapper.standard.string(forKey: "SecretMessage") {
+            secret.text = text
+        }
+        
+    }
+    
+    @objc func saveSecretMessage() {
+        guard secret.isHidden == false else { return }
+
+        KeychainWrapper.standard.set(secret.text, forKey: "SecretMessage")
+        secret.resignFirstResponder()
+        secret.isHidden = true
+        title = "Nothing to see here"
+    }
+    
+    @IBAction func authenticateTapped(_ sender: Any) {
+        unlockSecretMessage()
+    }
+    
     @objc func adjustForKeyboard(notification: Notification) {
         guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
 
