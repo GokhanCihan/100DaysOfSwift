e3cd28a8a3beafae3c0edcc46021297612c916a9
Gökhan Cihan <cihangokhan@gmail.com>
2022-10-30 15:02:57 +0300

    create Project29

---
+++ Project29-Exploding-Monkeys/Project29-Exploding-Monkeys/GameScene.swift
@@ -0,0 +1,17 @@
+//
+//  GameScene.swift
+//  Project29-Exploding-Monkeys
+//
+//  Created by Gökhan on 30.10.2022.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+}
