a39902aad11bfa06ddf0aaae39af29854c37cfcd
Gökhan Cihan <cihangokhan@gmail.com>
2023-01-28 22:56:07 +0300

    created card view variables and initializer

--- MilestoneProjects/CardPairing/CardPairing/CardView.swift
+++ MilestoneProjects/CardPairing/CardPairing/CardView.swift
@@ -7,6 +7,24 @@
 
 import UIKit
 
+enum Side {
+    case front
+    case back
+}
 class CardView: UIView {
+    var side = Side.front
+    
+    //front side of the card view
+    var kanji = UIImageView()
+    var reading = UILabel()
+    var meaningEN = UILabel()
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
     
 }
