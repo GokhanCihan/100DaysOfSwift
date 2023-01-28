8fcbee8532578893aacfe67128e87dfe204c35c9
Gökhan Cihan <cihangokhan@gmail.com>
2023-01-28 10:55:14 +0300

    update model

--- MilestoneProjects/CardPairing/CardPairing/Card.swift
+++ MilestoneProjects/CardPairing/CardPairing/Card.swift
@@ -7,8 +7,18 @@
 
 import Foundation
 
+//JLPT proficiency levels
+enum JLPT {
+    case N5
+    case N4
+    case N3
+    case N2
+    case N1
+}
+
 struct Card {
     let id = UUID()
+    var level: JLPT
     var reading: String
     var meaningEN: String
     var stringForImageURL: String    
