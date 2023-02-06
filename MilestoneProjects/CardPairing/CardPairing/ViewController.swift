39b96658afbf0899081704953aff3b6ec2b03c33
Gökhan Cihan <cihangokhan@gmail.com>
2023-02-06 15:58:27 +0300

    configure layout

--- MilestoneProjects/CardPairing/CardPairing/ViewController.swift
+++ MilestoneProjects/CardPairing/CardPairing/ViewController.swift
@@ -8,12 +8,62 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let amountOfCards = 16
+    var cards = [Card]()
+    var cardViews = [CardView]()
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.configureData()
+        self.configureLayout()
     }
 
-
+    func configureData() {
+        for _ in 0..<self.amountOfCards {
+            let card = Card(
+                level: .N5,
+                reading: "READING",
+                meaningEN: "MEANING",
+                stringForImageURL: "URLSTRING"
+            )
+            let cardView = CardView()
+            cardView.name.text = card.reading
+            self.cards.append(card)
+            self.cardViews.append(cardView)
+        }
+    }
+    
+    func configureLayout() {
+        let viewWidthUsed = self.view.safeAreaLayoutGuide.layoutFrame.width - 100
+        let viewHeightUsed = self.view.safeAreaLayoutGuide.layoutFrame.height - 100
+        print("safeArea W: \(viewWidthUsed)\nsafeArea H: \(viewHeightUsed)")
+        
+        let cardWidth = 0.2 * viewWidthUsed
+        let cardHeight = 0.2 * viewHeightUsed
+        print("card W: \(cardWidth)\ncard H: \(cardHeight)")
+        
+        let marginFromSibling = CGPoint(x: 0.25 * cardWidth, y: 0.25 * cardHeight)
+        print("marginFromSibling X: \(marginFromSibling.x)\nmarginFromSibling Y: \(marginFromSibling.y)")
+        
+        // How many points away from screen edge the first line of cards should be
+        let marginFromSafeArea = CGPoint(
+            x: (viewWidthUsed - 4 * cardWidth - 3 * marginFromSibling.x) / 2 + 50,
+            y: (viewHeightUsed - 4 * cardHeight - 3 * marginFromSibling.y) / 2 + 50
+        )
+        print("marginFromSafeArea X: \(marginFromSafeArea.x)\nmarginFromSafeArea Y: \(marginFromSafeArea.y)\n\n\n")
+        for row in 0..<4{
+            for column in 0..<4{
+                let index = 4 * row + column
+                cardViews[index].frame = CGRect(
+                    x: Int(marginFromSafeArea.x + CGFloat(column) * (cardWidth + marginFromSibling.x)),
+                    y: Int(marginFromSafeArea.y + CGFloat(row) * (cardHeight + marginFromSibling.y)),
+                    width: Int(cardWidth),
+                    height: Int(cardHeight)
+                )
+                self.view.addSubview(cardViews[index])
+                self.cardViews[index].configureView()
+            }
+        }
+    }
 }
 
