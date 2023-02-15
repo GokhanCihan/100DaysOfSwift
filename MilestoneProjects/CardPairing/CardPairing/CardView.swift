5cb169292200bc5587df648f6496f1f8ac52044c
Gökhan Cihan <cihangokhan@gmail.com>
2023-02-16 01:46:39 +0300

    update view

--- MilestoneProjects/CardPairing/CardPairing/CardView.swift
+++ MilestoneProjects/CardPairing/CardPairing/CardView.swift
@@ -12,59 +12,61 @@ enum Side {
     case back
 }
 class CardView: UIView {
-    var side = Side.front
-    var pairsID: UUID?
-    
-    //front side of the card
+    var side = Side.back
+
+    // front side of the card
     var frontSideView = UILabel()
-    
-    //back side of the card
+
+    // back side of the card
     var backSideView = UIImageView()
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     override init(frame: CGRect) {
-        super.init(frame: frame)
+        super.init(frame: .zero)
         self.isUserInteractionEnabled = true
     }
-    
 }
 extension CardView {
     func configureView() {
-        self.layer.borderColor = UIColor.gray.cgColor
-        self.layer.backgroundColor = UIColor.secondarySystemBackground.cgColor
-        self.layer.borderWidth = 1
-        
-        if side == .back {
+        if side == .front {
             frontSideView.isHidden = true
             backSideView.isHidden = false
-        }else {
+        } else {
             frontSideView.isHidden = false
             backSideView.isHidden = true
         }
-        
+
+        self.layer.borderColor = UIColor.lightGray.cgColor
+        self.layer.borderWidth = 0.5
+
         backSideView.translatesAutoresizingMaskIntoConstraints = false
+        backSideView.contentMode = .scaleAspectFill
+        backSideView.clipsToBounds = true
+
         backSideView.image = UIImage(named: "BackSide.png")
         self.addSubview(backSideView)
-        
+
         frontSideView.translatesAutoresizingMaskIntoConstraints = false
+        frontSideView.layer.backgroundColor = UIColor.white.cgColor
+        frontSideView.font = .systemFont(ofSize: 80, weight: .thin)
+
         frontSideView.adjustsFontSizeToFitWidth = true
-        frontSideView.font = UIFont.boldSystemFont(ofSize: 80)
         frontSideView.textAlignment = .center
         self.addSubview(frontSideView)
-        
+
         // subviews' constraints
         NSLayoutConstraint.activate([
             frontSideView.topAnchor.constraint(equalTo: self.topAnchor),
             frontSideView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
             frontSideView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
-            frontSideView.heightAnchor.constraint(equalTo: self.heightAnchor),
+            frontSideView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
             backSideView.topAnchor.constraint(equalTo: self.topAnchor),
             backSideView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
             backSideView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
-            backSideView.heightAnchor.constraint(equalTo: self.heightAnchor)
+            backSideView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
         ])
     }
 }
