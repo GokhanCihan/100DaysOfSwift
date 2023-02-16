c853df6bc141f233087aadf0e7a2c18238323590
Gökhan Cihan <cihangokhan@gmail.com>
2023-02-16 20:57:49 +0300

    create mock data

---
+++ MilestoneProjects/CardPairing/CardPairing/MockData.swift
@@ -0,0 +1,13 @@
+//
+//  MockData.swift
+//  CardPairing
+//
+//  Created by Gökhan on 16.02.2023.
+//
+
+import Foundation
+
+struct MockData {
+    let symbol = ["一", "右", "雨", "円", "王", "音", "下", "火", "花", "貝", "学", "気", "休", "玉", "金", "九", "空", "月", "犬", "見", "五", "口", "校", "左", "三", "山", "四", "子", "糸", "字", "耳", "七", "車", "手", "十", "出", "女", "小", "上", "森", "人", "水", "正", "生", "青", "石", "赤", "先", "千", "川", "早", "草", "足", "村", "大", "男", "竹", "中", "虫", "町", "天", "田", "土", "二", "日", "入", "年", "白", "八", "百", "文", "本", "名", "木", "目", "夕", "立", "力", "林", "六"]
+    let reading = ["ichi", "u", "ame", "en", "ou", "oto", "shita", "hi", "hana", "kai", "gaku", "ki", "yasu", "tama", "kin", "kyuu", "sora", "tsuki", "inu", "mi", "go", "kuchi", "kou", "hidari", "san", "yama", "shi", "ko", "ito", "ji", "mimi", "shichi", "kuruma", "te", "juu", "de", "onna", "chiisai", "ue", "mori", "hito", "mizu", "tadashii", "ikiru", "ao", "ishi", "aka", "saki", "sen", "kawa", "hayai", "kusa", "ashi", "mura", "ookii", "otoko", "take", "naka", "mushi", "machi", "ten", "ta", "tsuchi", "ni", "hi", "hairu", "toshi", "shiroi", "hachi", "hyaku", "bun", "hon", "na", "ki", "me", "yuu", "tatsu", "chikara", "hayashi", "roku"]
+}
